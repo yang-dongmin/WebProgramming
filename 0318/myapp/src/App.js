758458b1615657b58a053d615801eb43326b728e
양동민 <yangdongmin@yangdongmin-ui-MacBookAir.local>
2025-03-21 15:00:48 +0900

    0318 webprogramming_1

---
+++ 0318/myapp/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Commentlist from "./chapter_05/Commentlist";
+// Clock 컴포넌트 불러오기
+function App() {
+  return (
+    <div>
+      <Commentlist />
+    </div>
+  );
+}
+export default App;
