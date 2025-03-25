2c2f20f5aea4875f6bebdce94609f935d0bfe4f9
양동민 <yangdongmin@yangdongmin-ui-MacBookAir.local>
2025-03-25 11:40:25 +0900

    0325

---
+++ 0325/my-app/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CommentList from './chapter_05/Commentlist';
+function App() {
+  return (
+    <CommentList />
+  );
+}
+export default App;
---
+++ 0325/my-app/src/index.js
@@ -0,0 +1,13 @@
+//// #CommentList
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// const root = document.getElementById("root");
+// ReactDOM.createRoot(root).render(<App />);
+
+// #NotificationList
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NotificationList from './chapter_06/NotificationList';
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<NotificationList />);
